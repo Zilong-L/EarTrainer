0d4db5a7400886c3222a6223e7c84bafcfd1176f
lzl <875159954@qq.com>
2024-06-15 00:31:31 +0800

    try no baseurl

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react() ],
-  base: '/ChordTrainer',
+  plugins: [react()],
+  // base: '/ChordTrainer',
   build: {
-    target: 'esnext',
+    target: "esnext",
   },
   resolve: {
     alias: {
-      '@components': '/src/Components',
-      '@utils': '/src/utils',
-      '@assets': '/src/assets',
+      "@components": "/src/Components",
+      "@utils": "/src/utils",
+      "@assets": "/src/assets",
       // Add more aliases as needed
-    }
+    },
   },
-})
+});
