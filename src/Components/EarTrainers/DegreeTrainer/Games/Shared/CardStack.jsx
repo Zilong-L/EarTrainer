c56e21525015d31f0912a760efefbbed69211941
lzl <875159954@qq.com>
2025-02-19 15:54:39 +0800

    temp animation

--- src/Components/EarTrainers/DegreeTrainer/Games/Shared/CardStack.jsx
+++ src/Components/EarTrainers/DegreeTrainer/Games/Shared/CardStack.jsx
@@ -1,10 +1,10 @@
 import React, { useState, useRef, useEffect, useCallback } from 'react';
 import * as Tone from 'tone';
 import { isCorrect } from '@utils/GameLogics';
-import { SolfegeMapping,shortcuts } from '@components/EarTrainers/DegreeTrainer/Constants';
+import { SolfegeMapping, shortcuts } from '@components/EarTrainers/DegreeTrainer/Constants';
 import Button from '@components/SharedComponents/Button';
 import { calculateDegree, } from '../../../../../utils/GameLogics';
-const CardStack = ({ 
+const CardStack = ({
   currentNote,
   disabledNotes,
   filteredNotes,
@@ -18,12 +18,13 @@ const CardStack = ({
   gameState
 }) => {
   const [hoveredIndex, setHoveredIndex] = useState(null);
+  const [exitingIndex, setExitingIndex] = useState(null);
   const [time, setTime] = useState(Date.now());
   const timeOffsets = useRef([]);
   const [hoverRotations, setHoverRotations] = useState(Array(12).fill(0).map(() => ({ rotateX: 0, rotateY: 0 })));
 
   useEffect(() => {
-    timeOffsets.current = filteredNotes.slice(0, 12).map(() => Math.random() * 10000);
+    timeOffsets.current = filteredNotes.slice(0, 12).map(() => Math.random() * 5);
   }, [filteredNotes]);
 
   useEffect(() => {
@@ -38,7 +39,7 @@ const CardStack = ({
     const centerY = rect.top + rect.height / 2;
     const mouseX = event.clientX;
     const mouseY = event.clientY;
-    
+
     const deltaX = (mouseX - centerX) / (rect.width / 2);
     const deltaY = (mouseY - centerY) / (rect.height / 2);
 
@@ -50,14 +51,23 @@ const CardStack = ({
       [index]: { rotateX, rotateY }
     }));
   }, []);
-
-  const handleMouseLeave = useCallback((index) => {
+  const handleMouseEnter = (index) => {
+    setHoveredIndex(index);
+  };
+  
+  const handleMouseLeave = (index) => {
+    setExitingIndex(index);
     setHoveredIndex(null);
-  }, []);
+    setTimeout(() => {
+      setExitingIndex(null);
+    }, 2000); // Matches animation duration
+  };
+  
+
 
   return (
     <div>
-      <div className={`hidden lg:${gameState=='end'?'hidden':''} lg:flex items-center justify-center h-64 mb-4 relative`}>
+      <div className={`hidden lg:${gameState == 'end' ? 'hidden' : ''} lg:flex items-center justify-center h-64 mb-4 relative`}>
         <div className="flex h-full items-center" style={{ width: `${Math.min(filteredNotes.length, 12) * 15}%` }}>
           {filteredNotes.slice(0, 12).map((note, index) => {
             const noteName = Tone.Frequency(Tone.Frequency(rootNote).toMidi() + note.distance, 'midi').toNote();
@@ -66,33 +76,26 @@ const CardStack = ({
               noteName.slice(0, -1) === disabledNote.slice(0, -1)
             );
             const totalCards = filteredNotes.length;
-            
+
             const minDistance = 15;
             const maxDistance = 20;
-            const cardDistance = maxDistance - (maxDistance - minDistance) * (totalCards/12);
+            const cardDistance = maxDistance - (maxDistance - minDistance) * (totalCards / 12);
             const distance = index - hoveredIndex;
-            const offsetMagnitude = cardDistance * 3*(totalCards/12) * Math.exp(-0.1*Math.pow(Math.abs(distance), 2));
+            const offsetMagnitude = cardDistance * 3 * (totalCards / 12) * Math.exp(-0.1 * Math.pow(Math.abs(distance), 2));
 
             const offsetX = Math.sign(distance) * offsetMagnitude;
             const maxRotation = 15;
             const maxVerticalOffset = 100;
-            const positionRatio = (index - (totalCards-1 ) / 2) / (totalCards / 2);
+            const positionRatio = (index - (totalCards - 1) / 2) / (totalCards / 2);
 
             const rotation = maxRotation * positionRatio;
-            const verticalOffset = (0.4+0.4*Math.pow(positionRatio,2)) * maxVerticalOffset;
+            const verticalOffset = (0.4 + 0.4 * Math.pow(positionRatio, 2)) * maxVerticalOffset;
             const mouseRotation = hoverRotations[index];
-            const timeFactor = (time + timeOffsets.current[index]) * 0.001;
-            const baseRotateX = Math.sin(timeFactor) * 15;
-            const baseRotateY = Math.cos(timeFactor) * 15;
-            
-            const rotateX =  baseRotateX;
-            const rotateY =  baseRotateY;
-        
-            let cardTransform = hoveredIndex === null
-              ? `rotateZ(${rotation}deg) translateY(${verticalOffset}%) rotateX(${rotateX}deg) rotateY(${rotateY}deg)`
-              : `rotateZ(${rotation}deg) translateY(${verticalOffset}%) rotateX(${rotateX}deg) rotateY(${rotateY}deg) translateX(${offsetX}%)`;
 
-            if(hoveredIndex === index&&mouseRotation){
+
+            let cardTransform = '';
+
+            if (hoveredIndex === index && mouseRotation) {
               cardTransform = ` translateY(4rem) rotateX(${mouseRotation.rotateX}deg) rotateY(${mouseRotation.rotateY}deg) translateX(${offsetX}%) scale(1.3)`;
             }
 
@@ -100,29 +103,34 @@ const CardStack = ({
             if (isHandfree && isCorrectAnswer && index !== hoveredIndex) {
               setHoveredIndex(index);
             }
-            
+
             return (
               <div
                 key={note.name}
-                onMouseMove={(e) => handleMouseMove(e, index)}
-                onMouseLeave={() => handleMouseLeave(index)}
-                className={`
+                className={`  
+                  ${hoveredIndex === index ? 'animate-hoverCard' :exitingIndex==index ?'animate-hoverCardReverse': 'animate-floatCard'}
                   absolute h-32 w-24  md:h-[14rem] md:w-[10rem] rounded-xl shadow-[4px_1px_0px_rgba(0,0,0,0.6),0_2px_4px_-2px_rgba(0,0,0,0.2)]
                   flex flex-col items-center justify-between py-4 text-2xl md:text-3xl font-bold
                   transition-transform duration-300 ease-[cubic-bezier(0.34,1.56,0.64,1)]  
-                  ${hoveredIndex === index ? `shadow-[8px_10px_1px_rgba(0,0,0,0.7)] z-50` :''}
+                  ${hoveredIndex === index ? `shadow-[8px_10px_1px_rgba(0,0,0,0.7)] z-50` : ''}
                   z-[${cardZIndex}]
-                  ${isCorrectAnswer && isAdvance !=='No'? 'bg-gradient-to-b from-green-600 to-green-400' : 'bg-bg-accent text-text-primary'}
+                  ${isCorrectAnswer && isAdvance !== 'No' ? 'bg-gradient-to-b from-green-600 to-green-400' : 'bg-bg-accent text-text-primary'}
                   ${isDisabled ? 'opacity-50 pointer-events-none' : ''}
-                `}
-                style={{
-                  left: `calc(50% - 3rem + ${(index - filteredNotes.length / 2) * cardDistance}%)`,
-                  transform: cardTransform,
-                  transformOrigin: 'bottom center',
-                  willChange: 'transform',
-                  perspective: '1000px',
-                }}
-                onMouseEnter={() => setHoveredIndex(index)}
+                  `}
+                  style={{
+                    left: `calc(50% - 3rem + ${(index - filteredNotes.length / 2) * cardDistance}%)`,
+                    transformOrigin: 'bottom center',
+                    willChange: 'transform',
+                    perspective: '1000px',
+                    '--z-rotate': `${rotation}deg`,
+                    '--base-translateY': `${verticalOffset}%`,
+                    '--hover-rotateX': `${mouseRotation?.rotateX ?? 10}deg`,
+                    '--hover-rotateY': `${mouseRotation?.rotateY ?? 10}deg`,
+                    '--hover-offsetX': `${offsetX}%`
+                  }}
+                onMouseMove={(e) => handleMouseMove(e, index)}
+                onMouseLeave={() =>  handleMouseLeave(index)}
+                onMouseEnter={() => handleMouseEnter(index)}
                 onClick={() => {
                   setHoveredIndex(index);
                   setActiveNote(noteName);
@@ -140,7 +148,7 @@ const CardStack = ({
           })}
         </div>
       </div>
-      <div className={`grid grid-cols-3 gap-4 mb-4 lg:hidden ${gameState=='end'?'hidden':''}`}>
+      <div className={`grid grid-cols-3 gap-4 mb-4 lg:hidden ${gameState == 'end' ? 'hidden' : ''}`}>
         {filteredNotes.map((note) => {
           const noteName = Tone.Frequency(Tone.Frequency(rootNote).toMidi() + note.distance, 'midi').toNote();
           const isCorrectAnswer = isCorrect(noteName, currentNote) && isAdvance !== 'No';
